4559850384f9b2a63a3a1929f308db05ac9cbb90
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-08-24 18:27:59 +0530

    Viewing the Conferences

--- src/GlobomanticsCookieAuth.Web/Program.cs
+++ src/GlobomanticsCookieAuth.Web/Program.cs
@@ -1,7 +1,27 @@
+using GlobomanticsCookieAuth.Web.Interfaces;
+using GlobomanticsCookieAuth.Web.Repositories;
+using Microsoft.AspNetCore.Authentication.Cookies;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddRazorPages();
+builder.Services.AddScoped<IConferenceRepository, ConferenceRepository>();
+builder.Services.AddScoped<IProposalRepository, ProposalRepository>();
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+
+builder.Services.AddAuthentication(o =>
+{
+    o.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+    //o.DefaultChallengeScheme = GoogleDefaults.AuthenticationScheme;           
+})
+    .AddCookie();
+//.AddGoogle(o =>
+//{
+//    o.ClientId = "Your_ClientID";
+//    o.ClientSecret = "Your_Secret";
+//});
 
 var app = builder.Build();
 
@@ -14,14 +34,19 @@ if (!app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
 app.UseStaticFiles();
 
 app.UseRouting();
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Conference}/{action=Index}/{id?}");
+
+//app.MapControllerRoute("default", "{controller=Conference}/{action=Index}/{id?}");
 
 app.Run();
